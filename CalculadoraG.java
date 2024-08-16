8aa6389fc00d7c985a24ea5bd22a6ea2ca4448ad
valeng1133 <est.yulianav.gomez@unimilitar.edu.co>
2024-08-16 12:54:42 -0500

    Creación de la interfaz de la Calcuadora
    En esta parte del proyecto, cree la interfaz gráfica de la calculadora y asigne nombres a los botones para que al momento de programarlos y revisar el código pueda saber a qué función u operación pertenece cada uno.

---
+++ CalculadoraG.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadorag;
+
+/**
+ *
+ * @author Administrador
+ */
+public class CalculadoraG {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
